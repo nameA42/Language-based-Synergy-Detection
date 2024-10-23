87fdc097af546f860b1630e1fbffaf2be6badf1c
unknown <bbateni@ucsc.edu>
2024-10-23 11:21:31 -0700

    Change chat_example to use OpenAI instead of Qwen, use TextUtil for color changes in chat_example

--- chat_example.py
+++ chat_example.py
@@ -1,11 +1,9 @@
 from llm_connector import OpenAIChat, QWENChat
+from utility import TextUtil
 
 if __name__=="__main__":
-    YELLOW = '\033[93m'
-    RESET = '\033[0m'
-    # chat = OpenAIChat(OpenAIChat.OpenAIModel.GPT_4O_mini)
-    chat = QWENChat(QWENChat.QWENModel.QWEN2_5__0_5B)
+    chat = OpenAIChat(OpenAIChat.OpenAIModel.GPT_4O_mini)
+    # chat = QWENChat(QWENChat.QWENModel.QWEN2_5__0_5B)
     while True:
         prompt = input()
-        print(f"{YELLOW}{prompt}{RESET}")
-        print(chat.ask(prompt))
+        print(TextUtil.get_colored_text(chat.ask(prompt), TextUtil.TEXT_COLOR.Yellow))
